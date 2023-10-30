d56c6560017f2189f5414d45929333163fc7c458
Sergii Butenko <sergii.butenko@globallogic.com>
2023-10-30 09:59:58 +0100

    Added github api client

--- src/applications/github/api/github_api.py
+++ src/applications/github/api/github_api.py
@@ -1,5 +1,28 @@
-class GitHubAPI:
+import requests
+
+
+class GitHubAPIClient:
     """Current class contains every API call we use in tests"""
     
     def __init__(self) -> None:
         pass
+
+    def search_repos(self, repo_name):
+        print("Sending request to url https://api.github.com/search/repositories")
+        r = requests.get("https://api.github.com/search/repositories", params={'q': repo_name})
+        print(f"Responce retriewed {r}")
+
+        body = r.json()
+
+        return body
+    
+
+    def search_commits(self, commit_hash):
+        print("Sending request to url https://api.github.com/search/commit")
+        r = requests.get("https://api.github.com/search/commit", params={'q': commit_hash})
+        print(f"Responce retriewed {r}")
+
+        body = r.json()
+
+        return body
+    
--- tests/github/api/test_api.py
+++ tests/github/api/test_api.py
@@ -1,17 +1,34 @@
-from src.config.config_easy import ConfigEasy
-from src.config.config_harder import config
+from src.applications.github.api.github_api import GitHubAPIClient
 
 
-def test_api():
-    print(ConfigEasy.request_timeout)
-    print(config.BASE_URL_UI)
-    print(config.get('BASE_URL_UI')) # java
+def test_search_for_existing_repo():
+    github_api_client = GitHubAPIClient()
 
-def test_api():
-    print(ConfigEasy.request_timeout)
+    existing_repo_name = 'sergii'
+    repos = github_api_client.search_repos(existing_repo_name) # list/array returned
+
+    print("Checking total count is not 0")
     
-def test_api():
-    print(ConfigEasy.request_timeout)
+    assert repos['total_count'] != 0  # some elements exists in array/list
+
+
+def test_search_for_nonexisting_repo():
+    github_api_client = GitHubAPIClient()
     
-def test_api():
-    print(ConfigEasy.request_timeout)
+    nonexisting_repo_name = 'lnasdklfnkjanskdjfnkjabsdjhfuyagsuygfuyagsduyfgauysdf'
+    repos = github_api_client.search_repos(nonexisting_repo_name) # list/array returned
+
+    print("Checking total count is 0")
+    
+    assert repos['total_count'] == 0  # some elements exists in array/list
+
+
+def test_search_for_nonexisting_repo():
+    github_api_client = GitHubAPIClient()
+    
+    nonexisting_commit_hasg = 'lnasdklfnkjanskdjfnkjabsdjhfuyagsuygfuyagsduyfgauysdf'
+    repos = github_api_client.search_commits(nonexisting_commit_hasg) # list/array returned
+
+    print("Checking total count is 0")
+    
+    assert 0 == 0  # some elements exists in array/list
