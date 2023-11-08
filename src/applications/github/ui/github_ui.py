33fea870b6e6b931b2f0dcefb4bbac8407a04b49
Sergii Butenko <sergii.butenko@globallogic.com>
2023-11-08 10:03:33 +0100

    added page object

--- src/applications/github/ui/github_ui.py
+++ src/applications/github/ui/github_ui.py
@@ -1,5 +1,41 @@
-class GitHubUI:
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+
+
+class GitHubUILoginPage:
     """Current class contains every UI usage we use in tests"""
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, browser_name) -> None:
+        if browser_name == 'ff':
+            self.driver = webdriver.Firefox()
+        elif browser_name == 'chrome':
+            pass
+
+    def navigate_to_page(self):
+        self.driver.get("https://github.com/login")
+        # waiters that some data exists
+
+
+    def try_to_login(self, username, password):
+        login_fld = self.driver.find_element(By.ID ,"login_field")
+        #waiter to be added
+
+        login_fld.send_keys(username)
+        
+        pass_fld = self.driver.find_element(By.ID ,"password") 
+        #waiter to be added
+        pass_fld.send_keys(password)
+
+        # click button
+        login_fld = self.driver.find_element(By.NAME ,"commit")
+        #waiter to be added
+
+        login_fld.click()
+
+    def check_login_error_message(self):
+        error_msg = self.driver.find_element(By.ID ,"js-flash-container") # replace with different identifier e.g. Text of error
+
+        return error_msg is not None
+
+    def close_browser(self):
+        self.driver.quit()
--- tests/github/ui/test_ui.py
+++ tests/github/ui/test_ui.py
@@ -1,6 +1,9 @@
+import pytest
+
 from selenium import webdriver 
 import time
 from selenium.webdriver.common.by import By
+from src.applications.github.ui.github_ui import GitHubUILoginPage
 
 
 def test_github_login_negative():
@@ -28,3 +31,59 @@ def test_github_login_negative():
     time.sleep(5)
     assert error_msg is not None
     
+
+
+#### test with tear_up/teardown but whout page object
+@pytest.fixture
+def github_login():
+    # tear_up foreach
+    # open the browser
+    driver = webdriver.Firefox()  
+    # navigate to login page
+    driver.get("https://github.com/login")
+
+    # return login page
+    yield driver
+
+    # tear_down foreach
+    # close the browser --post set after EACH
+    driver.quit()
+
+
+
+def test_github_login_negative_fixture(github_login):
+    login_fld = github_login.find_element(By.ID ,"login_field")
+    login_fld.send_keys("wrong email")
+    
+    pass_fld = github_login.find_element(By.ID ,"password") 
+    pass_fld.send_keys("wrong pass")
+
+    # click button
+    login_fld = github_login.find_element(By.NAME ,"commit")
+    login_fld.click()
+    
+    # check error message
+    error_msg = github_login.find_element(By.ID ,"js-flash-container") # replace with different identifier e.g. Text of error
+    time.sleep(5)
+
+    assert error_msg is not None
+
+
+@pytest.fixture
+def github_login_page_object():
+    # tear_up foreach
+    gitlab_login_page = GitHubUILoginPage()
+    gitlab_login_page.navigate_to_page()
+    
+    # return login page
+    yield gitlab_login_page
+
+    # tear_down foreach
+    # close the browser --post set after EACH
+    gitlab_login_page.close_browser()
+
+
+def test_github_login_negative_page_object(github_login_page_object):
+    github_login_page_object.try_to_login("kjasbdkjfsa", "ksjhdkjfsdf")
+
+    assert github_login_page_object.check_login_error_message()
